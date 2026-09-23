da679d5305d468a6ce7b5495d96b534f3b272222
Milkii <mdagne12@terpmail.umd.edu>
2026-09-23 14:22:59 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # memo[i] represents the number of ways you can reach stair i
+        memo = { 1:1, 2:2 }
+        def dp(i):
+            if i not in memo: 
+                num_ways_to_reach_i = dp(i - 1) + dp(i - 2)
+                memo[i] = num_ways_to_reach_i
+
+            return memo[i]
+            
+        return dp(n)
