a6236247d5407ab4de3c202434fa06ff3331ef5e
Milkii <mdagne12@terpmail.umd.edu>
2026-09-23 14:40:11 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        dp = [ 0, nums[0]]
+
+        for i in range(1, len(nums)):
+            dp.append(max(dp[-1], dp[-2] + nums[i]))
+
+        return dp[-1]
+            
+
+        
